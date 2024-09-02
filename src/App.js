8d82432598eee55044ea22623c69cd8b9f7bf711
omar <omarhd994@gmail.com>
2024-09-02 23:34:22 +0200

    exchange app 2-09-24

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { createChart } from 'lightweight-charts';
 import './App.css';
-import imageCrypto from "./crypto.jpg"
+import imageCrypto from "./images/CryptoLogo.png"
 import { useEffect, useState, useCallback } from 'react';
 
 function App() {
   const [crypto, setCrypto] = useState("");
   const [showCrypto, setShowCrypto] = useState("");
   const [loading, setLoading] = useState(false);
-  const [lineSeries, setLineSeries] = useState(null)
-  const [cambio, setCambio] = useState("")
-  const [show, setShow] = useState(false)
-  const [cronometro, setCronometro] = useState(0)
-  const [activo,setActivo] = useState(false)
-  const [cryptoList, setCryptoList] = useState([])
+  // const [cambio, setCambio] = useState("")
+  // const [show, setShow] = useState(false)
+  // const [cronometro, setCronometro] = useState(0)
+  // const [activo,setActivo] = useState(false)
+  // const [cryptoList, setCryptoList] = useState([])
 
   // Función para manejar el cambio en el input de la criptomoneda
   const handleCrypto = (e) => {
     setCrypto(e.target.value);
   }
 
-  // Función para manejar la llamada a la API y el estado de carga
-  const handleShowInfo = useCallback(() => {
+  // Función para manejar la llamada a la API y el estado de carga   -   //useCallback => memoriza la funcion para no recrearla again o solo cuando [crypto] cambia
+  const handleShowInfo = useCallback(() => { 
     setLoading(true);
     fetch(`https://api.coinbase.com/v2/exchange-rates?currency=${crypto}`)
       .then(response => response.json())
@@ -41,36 +39,6 @@ function App() {
     handleShowInfo();
   }
 
-  const handleChart = () => {
-    setLoading(true)
-    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
-    .then(response => response.json())
-    .then(data => { 
-      const bitCoinData = data[0]
-      const prices = [{ time: new Date(bitCoinData.last_updated).getTime(), value: parseFloat(bitCoinData.current_price)  }]
-      prices.sort((a, b) => a.time - b.time);
-      lineSeries.setData(prices)
-     })
-     .catch(error => {
-        console.error("Error fetching chart data:", error)
-        setLoading(false)
-     })
-  }
-
-  useEffect(()=> {
-    const chart = createChart(document.getElementById("chart"), {
-      width: 600,
-      height: 300,
-    })
-
-    const series = chart.addLineSeries()    // Se aniade las lineas del grafico
-    setLineSeries(series)         // Se aniade la info en el estado chartData al grafico
-
-    return () => {
-      chart.remove()
-    }
-  }, [])
-
   useEffect(() => {
     // Si la longitud de la cadena es mayor o igual a 3, llamamos a handleShowInfo
     if (crypto.length >= 3)
@@ -90,7 +58,8 @@ function App() {
 
   /////////////////////////
 
-  useEffect(()=> {
+  {/* 
+    useEffect(()=> {
     let interval;
     
     activo ? 
@@ -103,28 +72,42 @@ function App() {
     return () => clearInterval(interval)    // useEffect tiene siempre un return, en este caso limpia interval
 
   }, [activo])
-    
+  
+  */}
+
   useEffect (() => {
   
       fetch("https://api.coincap.io/v2/assets")
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        setCryptoList(data.data)
+//setCryptoList(data.data)
       }
       )
     
   }, [])
   
+  const handleSubmitJuegos = (e) => {
+    e.preventDefault();
 
+    let juegos = {
+      id: new Date().getTime(),
+      titulo: e.target.titulo.value,
+      descripcion: e.target.descripcion.value,
+    }
+    console.log(juegos)
+  }
   
   return (
-    <>
+
+
+
+    <div className='exchange'>
       <img src={imageCrypto} alt='crypto' width="220" height="220" className='imageCrypto'/>
       <br/>
 
-      <input type='text' onChange={handleCrypto} />
-      <button onClick={handleClick}>Show info</button>
+      <input type='text' onChange={handleCrypto} placeholder='Cryptocurrency Name'/>
+      <button className="showInfo" onClick={handleClick}>Show info</button>
 
       {loading === true ? (
         <div>
@@ -135,34 +118,35 @@ function App() {
           <>
 
             {crypto.length < 3 &&
-              <h2> Put the symbol of the crypto </h2>}
+              <h2 className='symbolName'> Put the symbol of the crypto </h2>}
 
             {showCrypto  ? (
               <div>
-                <h2>The exchange rate for {crypto} is: {showCrypto} </h2>
+                <h2 className='rate'>The exchange rate for {crypto} is: {showCrypto} </h2>
               </div>)
               : (
-                <h2>No data available</h2>
+                <h2 className='rateNoData'>No data available</h2>
               )
             }
 
+            {/*
+
             <input type={show ? "text" : "password"}                //// show y tipo = mostrar password con valor y tipo = cambio
             onChange= {(e) => {setCambio(e.target.value)}} 
-            value={cambio} />
-            <button onClick={() => {setShow(!show)}}> 
+            value={cambio} placeholder='Enter Password'/>
+            <button className='showPassword' onClick={() => {setShow(!show)}}> 
             {show ? "Hide Password" : "Show Password"}</button>
 
             <br/>
             <button onClick={ () => {setActivo(!activo)}}> {activo ? "Detener Cronometro" : "Activar Cronometro"} </button>
             <br/>
             {cronometro}
-
-          
-  <button onClick={handleChart}>Show Chart</button>
-   <div id='chart'/>
+            
+             */}
   
   <>
 
+{/*
    {cryptoList.map((crypto) => (
     <div key={crypto.id}>
               <h2>{crypto.name}</h2>
@@ -170,16 +154,22 @@ function App() {
               <p>{crypto.marketCapUsd}</p>
            
             </div>
-   )
-
-   )
+*/} 
    
-   }
+   <h3>Juegos</h3>
+    <form onSubmit={handleSubmitJuegos}>
+      <input type='texto' name='titulo' placeholder='Titulo del juego'/>
+      <br/>
+      <textarea name='descripcion' placeholder='Descripcion'/>
+      <br/>
 
+      <input type='Submit' value="guardar"/>
+    </form>
+   
    </>
           </>
         )}
-    </>
+    </div>
   );
 }
 
